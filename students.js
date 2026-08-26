c308a8144c14c56b196a7d8dca57b6848d8942bc
BritishCouncil-del <britishcouncil719@gmail.com>
2026-08-26 18:48:51 +0300

    Remove old student data and update new student info

--- students.js
+++ students.js
@@ -1,27 +1,7 @@
 const students = {
-  // مثال طالب قديم
-  "12345": {
-    candidate: "67890",
-    candidateId: "C001",
-    name: "Ahmed Ali",
-    dob: "2000-05-15",
-    sex: "Male",
-    examDate: "2026-08-20",
-    overall: "7.5",
-    listening: "7.0",
-    reading: "8.0",
-    writing: "7.0",
-    speaking: "7.5",
-    trfNumber: "TRF123456",
-    centre: "EG001",
-    centreName: "British Council Cairo",
-    image: "ahmed.png"
-  },
-
-  // الطالب الجديد من تقرير الـ IELTS
   "26EG009471ABDA001A": {
-    candidate: "009471",              // رقم المرشح
-    candidateId: "A04468374",         // رقم الهوية
+    candidate: "009471",
+    candidateId: "A04468374",
     name: "AHMED ABDELAATY ABDELHALIM",
     dob: "1995-01-01",
     sex: "M",
